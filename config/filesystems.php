e8c9d7eb85b1e7b25c14cc17aa7b89658ecfe3b7
walee sebiiy <waleesebiiy@gmail.com>
2022-07-24 20:26:20 +0100

    remove azure driver

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,13 +55,6 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
-        'azure' => [
-            'driver' => 'azure',
-            'sasToekn' => env('AZURE_SAS_TOEKN'),
-            'container' => env('AZURE_CONTAINER'),
-            'url' => env('AZURE_URL'),
-            'throw' => false,
-        ],
     ],
 
     /*
