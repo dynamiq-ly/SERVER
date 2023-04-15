1b5756f6188c5ed221ddd322c8d8095742128d19
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2023-04-15 12:13:43 +0200

    adding medical assistance api

---
+++ database/migrations/2023_04_14_151517_create_medical_assistances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medical_assistances', function (Blueprint $table) {
+            $table->id();
+            $table->string('request');
+            $table->text('description');
+            $table->boolean('isAnsewred')->default(false);
+            $table->string('room_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medical_assistances');
+    }
+};
